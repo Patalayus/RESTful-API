415fac5c56762ec464c0b1469f6fa0f88a4dd05b
Patalayus <lukecharles222@gmail.com>
2020-10-25 17:08:11 +0000

    article return function

--- src/github.com/patalayus/RESTSimpleAPI/main.go
+++ src/github.com/patalayus/RESTSimpleAPI/main.go
@@ -2,20 +2,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 type Article struct {
-	Tytle string 'json:"Title"'
-	Desc string 'json:"desc"'
-	Content string 'json:"content"'
+	Title   string `json:"Title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
 }
 
 type Articles []Article
 
 func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test Title", Desc: "Test Description", Content: "Hello World"},
+	}
+
 	fmt.Println("Endpoint Hit: All Articles Endpoint")
 	json.NewEncoder(w).Encode(articles)
 }
@@ -27,6 +32,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
 
